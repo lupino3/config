6c420bee18e318d326149e179b7e028b7f60a4e4
Your Name <your@email.com>
2021-04-12 13:45:09 +0100

    feat: install useful software

--- setup.ps1
+++ setup.ps1
@@ -4,6 +4,17 @@ if (-not (Test-Path ~\.gitconfig)) {
     cp $PSScriptRoot\.gitconfig ~
 }
 
+# Install software. Use a very hacky sentinel file to skip if not needed.
+# Install the updated Windows Package Manager from https://github.com/microsoft/winget-cli/releases before running this section.
+if (-not (Test-Path $PSScriptRoot\.installed)) {
+    winget install git
+    winget install Microsoft.VisualStudioCode.User-x64
+    winget install Microsoft.VisualStudio.Enterprise
+    winget install Microsoft.WindowsTerminal
+    # winget install Microsoft.Office (the installer doesn't seem to be working)
+    Set-Content $PSScriptRoot\.installed "yo"
+}
+
 if (-Not (Test-Path ~\.local_gitconfig)) {
     cp $PSScriptRoot\.local_gitconfig_template ~\.local_gitconfig
     code ~\.local_gitconfig
