3e93b3d3ff7793e2dc3c7291292d1842048cd19c
Your Name <your@email.com>
2021-04-12 13:45:54 +0100

    fix: use extended commands instead of aliases

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 # Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 if (-not (Test-Path ~\.gitconfig)) {
-    echo "Copying .gitconfig"
-    cp $PSScriptRoot\.gitconfig ~
+    Write-Output "Copying .gitconfig"
+    Copy-Item $PSScriptRoot\.gitconfig ~
 }
 
 # Install software. Use a very hacky sentinel file to skip if not needed.
@@ -16,9 +16,9 @@ if (-not (Test-Path $PSScriptRoot\.installed)) {
 }
 
 if (-Not (Test-Path ~\.local_gitconfig)) {
-    cp $PSScriptRoot\.local_gitconfig_template ~\.local_gitconfig
+    Copy-Item $PSScriptRoot\.local_gitconfig_template ~\.local_gitconfig
     code ~\.local_gitconfig
-    echo "Please edit ~/.local_gitconfig, adding the [user] stanza (name, email)"
+    Write-Output "Please edit ~/.local_gitconfig, adding the [user] stanza (name, email)"
 }
 
 # From https://www.hanselman.com/blog/how-to-make-a-pretty-prompt-in-windows-terminal-with-powerline-nerd-fonts-cascadia-code-wsl-and-ohmyposh
@@ -27,9 +27,9 @@ Install-Module oh-my-posh -Scope CurrentUser
 Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 
 if (-Not (Test-Path $PROFILE)) {
-    cp profile-template.ps1 $PROFILE
+    Copy-Item profile-template.ps1 $PROFILE
 } else {
-    echo "$PROFILE exists. Check if the contents have the commands in profile-template.ps1"
+    Write-Output "$PROFILE exists. Check if the contents have the commands in profile-template.ps1"
 }
 
-echo "Install Cascadia Code PL and set it as the font for Windows Terminal"
+Write-Output "Install Cascadia Code PL and set it as the font for Windows Terminal"
