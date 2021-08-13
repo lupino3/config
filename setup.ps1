d172177a188c751d0d3377b59d8b7b73ed39f511
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2021-08-13 08:26:32 +0200

    tweak: small updates and add TODOs

--- setup.ps1
+++ setup.ps1
@@ -6,6 +6,7 @@ if (-not (Test-Path ~\.gitconfig)) {
 
 # Install software. Use a very hacky sentinel file to skip if not needed.
 # Install the updated Windows Package Manager from https://github.com/microsoft/winget-cli/releases before running this section.
+# TODO: use winget import JSON file.
 if (-not (Test-Path $PSScriptRoot\.installed)) {
     winget install git
     winget install Microsoft.VisualStudioCode.User-x64
@@ -28,10 +29,7 @@ if (-Not (Test-Path ~\.ssh\id_rsa)) {
     ssh-keygen
 }
 
-# From https://www.hanselman.com/blog/how-to-make-a-pretty-prompt-in-windows-terminal-with-powerline-nerd-fonts-cascadia-code-wsl-and-ohmyposh
-Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 
 if (-Not (Test-Path $PROFILE)) {
     Copy-Item profile-template.ps1 $PROFILE
@@ -39,4 +37,5 @@ if (-Not (Test-Path $PROFILE)) {
     Write-Output "$PROFILE exists. Check if the contents have the commands in profile-template.ps1"
 }
 
-Write-Output "Install Cascadia Code PL and set it as the font for Windows Terminal"
+Write-Output "Install FiraCode Nerd Font and set it as the font for Windows Terminal"
+Write-Output "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
